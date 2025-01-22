8ca4c5f6e9c7eb8b0c4f1d541dcd028e14769432
Valcisoca1 <valentina.cisternas@usm.cl>
2025-01-22 13:31:29 -0300

    Handling new location for assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource", 
+        generator: {
+          filename: "static/assets/[hash][ext]", 
+        },
+      });
+      return config;
+    },
+  };
+  
 
 export default nextConfig;
